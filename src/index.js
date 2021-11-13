c038e5c7e8a12476f02228558e1250aab2677959
darlington nwanna chukwuemeka <66736845+darlincode@users.noreply.github.com>
2021-11-13 14:06:04 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Component } from 'react'
+import FormFieldGroup from "./FormFieldGroup";
+import './styles.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: this.props.fields
+    }
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  handleOnChange(e, props) {
+    this.props.fields[e.key].value = e.value
+    props.onChangeCallback(this.props.fields)
+  }
+
+  handleOnSubmit(e, props) {
+    e.preventDefault()
+    props.onClickCallback()
+  }
+
+  render() {
+    return (
+      <form>
+        {this.props.fields.map((field, i) => {
+          return (
+            <FormFieldGroup
+              input={field}
+              key={i}
+              index={i}
+              onChangeCallback={(e) => this.handleOnChange(e, this.props)}
+            />
+          )
+        })}
+        <button className="is-primary is-block" onClick={e => this.handleOnSubmit(e, this.props)}>Sign In</button>
+      </form>
+    );
+  }
+}
+
+export default Form;
